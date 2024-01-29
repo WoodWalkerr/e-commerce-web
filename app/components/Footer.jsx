ae8280e676d746bd7643cb978a503115bc2bcb16
Melven <melvenbagolaque@gmail.com>
2024-01-30 00:06:00 +0800

    minor updates in footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,40 +4,22 @@ import Link from "next/link";
 import { footerLinks } from "../constants";
 
 const Footer = () => (
-  <footer className='max-w-[1440px] mx-auto flex flex-col text-black-100  mt-5 border-t border-gray-100'>
-    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
-      <div className='flex flex-col justify-start items-start gap-6'>
-        <Image src='/logo.png' alt='logo' width={118} height={18} className='object-contain' />
-        <p className='text-base text-gray-700'>
-        WheelsAway 2024 <br />
-          All Rights Reserved &copy;
-        </p>
-      </div>
-
-      <div className="footer__links">
-        {footerLinks.map((item) => (
-          <div key={item.title} className="footer__link">
-            <h3 className="font-bold">{item.title}</h3>
-            <div className="flex flex-col gap-5">
-              {item.links.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.url}
-                  className="text-gray-500"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-          </div>
-        ))}
+  <footer className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+      <div className="flex flex-col justify-start items-start gap-6">
+        <Link href="/">
+          <h1 className="text-2xl md:text-4xl font-bold">
+            Shop<span className="text-primary">Ease</span>
+          </h1>
+        </Link>{" "}
+        
       </div>
     </div>
 
-    <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-      <p>@2024 WheelsAway. All rights reserved</p>
+    <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+      <p>@2024 ShopEase. All rights reserved</p>
 
-      <div className="footer__copyrights-link">
+      <div className="flex-1 flex sm:justify-end justify-center max-sm:mt-4 gap-10">
         <Link href="/" className="text-gray-500">
           Privacy & Policy
         </Link>
